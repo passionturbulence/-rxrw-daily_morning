4a593f221faa676ce854395083e1849384962017
passionturbulence <104828273+passionturbulence@users.noreply.github.com>
2025-04-10 19:30:43 +0800

    Update main.py
    Signed-off-by: passionturbulence <104828273+passionturbulence@users.noreply.github.com>

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def get_weather(city):
     try:
         # 编码城市名
         encoded_city = quote(city)
-        url = f"http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city={encoded_city}"
+        url = f"https://apis.tianapi.com/tianqi/index?key=1267e3290f4f9c5610f868069394d955&101070201=101020100&type=1={encoded_city}"
         
         # 发送请求
         response = requests.get(url, timeout=5)
