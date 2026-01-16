19de5253be39b9d59decf5cfde9e0c58fe24096e
passionturbulence <104828273+passionturbulence@users.noreply.github.com>
2026-01-17 01:11:11 +0800

    Update main.py
    Signed-off-by: passionturbulence <104828273+passionturbulence@users.noreply.github.com>

--- main.py
+++ main.py
@@ -180,8 +180,8 @@ def get_random_color():
 if __name__ == "__main__":
     # 获取所有数据
     weather, temp, report_date, tips = get_weather(city)
-    days_count = get_days_count(start_date, "第一个纪念日")
-    second_days_count = get_days_count(second_date, "第二个纪念日")
+    days_count = get_days_count-1(start_date, "第一个纪念日")
+    second_days_count = get_days_count-1(second_date, "第二个纪念日")
     birthday_left = get_birthday_left()
     inspiration = get_inspiration()
     
